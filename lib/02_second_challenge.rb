cad0a6d62ebcc972e9e34f009389f09c89d853f1
Jason  Zhou <jzhou35@buffalo.edu>
2020-10-22 04:03:51 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,19 +1,4 @@
 def second_challenge
-  epic_tragedy = {
-    :montague => {
-      :patriarch => {:name => "Lord Montague", :age => "53"}
-      :matriarch => {:name => "Lady Montague", :age => "54"},
-      :hero => {:name => "Romeo", :age => "15", :status =>}
-      :hero_friends => []
-      }, 
-    :capulet => {
-      :patriarch => {},
-      :matriarch => {},
-      :hero => {},
-      :hero_friends => []
-      }
-  }
-  
   :montague => {
       :patriarch => {},
       :matriarch => {},
